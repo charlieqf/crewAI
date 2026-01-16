ea9fa852eab8ea585130a29b0eef0b9c64b39825
Charles Feng <charlieqf@gmail.com>
2026-01-16 22:58:15 +1100

    fix: add exports to handlers/__init__.py for wecom_callback.py

--- src/crewai_enterprise/server/handlers/__init__.py
+++ src/crewai_enterprise/server/handlers/__init__.py
@@ -4,4 +4,20 @@ WeCom Callback Server Handlers.
 This package contains modular handlers for different message types.
 """
 
-__all__ = []
+from src.crewai_enterprise.server.handlers.text_handler import (
+    BOT_CONFIG,
+    detect_bot_type,
+    is_clear_command,
+    process_text_message,
+    handle_clear_command,
+)
+from src.crewai_enterprise.server.handlers.file_handler import process_file_message
+
+__all__ = [
+    "BOT_CONFIG",
+    "detect_bot_type",
+    "is_clear_command",
+    "process_text_message",
+    "handle_clear_command",
+    "process_file_message",
+]
